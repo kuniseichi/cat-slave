eb3d366efb05f672db6430b4482818d5faab8cba
cbc <cbc0806com@gmail.com>
2019-02-19 16:14:39 +0800

    complete redis connect

--- model/init.go
+++ model/init.go
@@ -3,6 +3,7 @@ package model
 import (
 	"fmt"
 
+	"github.com/go-redis/redis"
 	"github.com/jinzhu/gorm"
 	"github.com/lexkong/log"
 	"github.com/spf13/viper"
@@ -10,6 +11,7 @@ import (
 
 type DataBase struct {
 	Mysql *gorm.DB
+	Redis *redis.Client
 }
 
 var DB *DataBase
@@ -17,20 +19,38 @@ var DB *DataBase
 func (db *DataBase) Init() {
 	DB = &DataBase{
 		Mysql: GetMysql(),
+		Redis: GetRedis(),
 	}
 }
 func (db *DataBase) Close() {
 	DB.Mysql.Close()
+	DB.Redis.Close()
 }
 
 func GetMysql() *gorm.DB {
-	return openDB(viper.GetString("docker_db.username"),
-		viper.GetString("docker_db.password"),
-		viper.GetString("docker_db.addr"),
-		viper.GetString("docker_db.name"))
+	return openMysql(viper.GetString("mysql.username"),
+		viper.GetString("mysql.password"),
+		viper.GetString("mysql.addr"),
+		viper.GetString("mysql.name"))
+}
+func GetRedis() *redis.Client {
+	return openRedis(viper.GetString("redis.addr"),
+		viper.GetString("redis.password"))
+}
+func openRedis(addr, password string) *redis.Client {
+	client := redis.NewClient(&redis.Options{
+		Addr:     addr,
+		Password: password, // no password set
+		DB:       0,        // use default DB
+	})
+	_, err := client.Ping().Result()
+	if err != nil {
+		log.Errorf(err, "redis connection failed")
+	}
+	return client
 }
 
-func openDB(username, password, addr, name string) *gorm.DB {
+func openMysql(username, password, addr, name string) *gorm.DB {
 
 	config := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8&parseTime=%t&loc=%s",
 		username,
@@ -46,13 +66,13 @@ func openDB(username, password, addr, name string) *gorm.DB {
 		log.Errorf(err, "Database connection failed. Database name: %s", name)
 	}
 
-	setupDB(db)
+	setupMysql(db)
 
 	return db
 
 }
 
-func setupDB(db *gorm.DB) {
+func setupMysql(db *gorm.DB) {
 	db.LogMode(viper.GetBool("gormlog"))
 	db.DB().SetMaxIdleConns(0)
 }
